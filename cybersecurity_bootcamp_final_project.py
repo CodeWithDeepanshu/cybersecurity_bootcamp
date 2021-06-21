f47f6a2de27d336210703575ebde4e169262a6c8
CodeWithDeepanshu <85788386+CodeWithDeepanshu@users.noreply.github.com>
2021-06-22 01:15:31 +0530

    Final Project
    You can see the weather details of your city using this code and it will also export the details in .txt format and whenever you will use this code again it will automatically update that .txt file named as result.txt

---
+++ cybersecurity_bootcamp_final_project.py
@@ -0,0 +1,32 @@
+# Cyber Security Bootcamp 
+
+# Final Project Made By Deepanshu Jain
+
+import requests
+from datetime import datetime
+
+api_key = '66aea0fa1d9ac3633970591670a7f9f7'
+location = input("Enter The City Name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime('%d %b %Y | %I:%M:%S %p')
+
+print('-------------------------------------------------------------------------------------------------------')
+print('Weather Stats for - {} || {}'.format(location.upper(),date_time))
+print('-------------------------------------------------------------------------------------------------------')
+
+print('Current Temperature is: {:.2f} deg C'.format(temp_city))
+print('Current Weather description: ',weather_desc)
+print('Current Humidity: ',hmdt, '%')
+print('Current Wind Speed: ',wind_spd ,'kmph')
+
+r=api_link   # This will export the result in .txt file
+with open('result.txt','wb') as f:
+    f.write(r.content)
